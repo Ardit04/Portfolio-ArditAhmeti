111230390562cff0e08f7009c40b125c2e45a6ec
Ardit04 <arditahmeti16@gmail.com>
2025-06-29 23:56:06 +0200

    update

--- api/data/experiences.php
+++ api/data/experiences.php
@@ -13,15 +13,7 @@ $experiences = [
         'description' => 'Design and develop full-stack web applications using HTML, CSS (TailwindCSS), JavaScript (React), PHP (Laravel), and MySQL. 
          Maintain and troubleshoot computer systems, provide technical support, and coordinate with suppliers for IT-related equipment and services.
          Focused on building scalable, user-friendly solutions and participating in the full software development lifecycle.',
-        'duration' => 'April 2025 – June 2025'
-    ],
-    [
-        'company' => 'Tectigon Academy',
-        'position' => 'Full Stack Developer Intern',
-        'description' => 'Completed an intensive full-stack development internship, gaining hands-on experience in modern web technologies such as 
-        HTML, TailwindCSS, JavaScript, React, PHP (Laravel), and MySQL. Built and deployed real-world web applications, practiced code versioning with Git,
-        and collaborated in team-based development projects.',
-        'duration' => 'January 2025 – April 2025'
+        'duration' => 'January 2025 – June 2025'
     ],
 
 ];
